6bc18d899f5bd82af1899ef824ed0266d803b535
Akashay Anand <akashayanand1@gmail.com>
2023-10-15 01:33:50 +0530

    implemented search functionalit with axios in JS

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const btn = document.getElementById('searchBtn');
+const searchInput = document.getElementById('searchInput');
 
-const btn = document.getElementById("searchBtn");
+// no need to reffere it will take 'id' directly ; code is working
+// const text = document.getElementById('text');
+// const definition = document.getElementById('definition'); 
 
+
+async function apiCall(word) {
+    const options = {
+        method: 'GET',
+        url: 'https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary',
+        params: {word: word},
+        headers: {
+          'X-RapidAPI-Key': 'a98681b022mshe045130ae3a435ep1ab093jsn91494ae077fc',
+          'X-RapidAPI-Host': 'dictionary-by-api-ninjas.p.rapidapi.com'
+        }
+      };
+    
+    try {
+        const response = await axios.request(options);
+        console.log(response);
+
+        text.innerHTML = response.data.word;
+        definition.innerHTML = response.data.definition;
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+btn.addEventListener('click',(e) => {
+    e.preventDefault();
+    const word = searchInput.value;  
+    apiCall(word);
+})
+
+  
